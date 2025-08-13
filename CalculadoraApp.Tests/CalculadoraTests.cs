5e38aa29acfaa492d093e1d53b9d9642ffbb2dff
Fabio Crusco da Silva <fcrusco77@gmail.com>
2025-08-13 19:04:22 -0300

    Add project files.

---
+++ CalculadoraApp.Tests/CalculadoraTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+
+namespace CalculadoraApp.Tests
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Somar_DeveRetornarSomaCorreta()
+        {
+            // Arrange
+            var calc = new CalculadoraApp.Calculadora();
+
+            // Act
+            var resultado = calc.Somar(5, 3);
+
+            // Assert
+            resultado.Should().Be(8);
+        }
+
+        [Fact]
+        public void Subtrair_DeveRetornarDiferencaCorreta()
+        {
+            var calc = new CalculadoraApp.Calculadora();
+
+            var resultado = calc.Subtrair(10, 4);
+
+            resultado.Should().Be(6);
+        }
+
+        [Fact]
+        public void Multiplicar_DeveRetornarProdutoCorreto()
+        {
+            var calc = new CalculadoraApp.Calculadora();
+
+            var resultado = calc.Multiplicar(7, 6);
+
+            resultado.Should().Be(42);
+        }
+
+        [Fact]
+        public void Dividir_ComDivisorZero_DeveLancarExcecao()
+        {
+            var calc = new CalculadoraApp.Calculadora();
+
+            var acao = () => calc.Dividir(10, 0);
+
+            acao.Should().Throw<DivideByZeroException>()
+                .WithMessage("Não é possível dividir por zero.");
+        }
+
+        [Theory]
+        [InlineData(10, 2, 5)]
+        [InlineData(9, 3, 3)]
+        [InlineData(7, 2, 3.5)]
+        public void Dividir_DeveRetornarQuocienteEsperado(int a, int b, double esperado)
+        {
+            var calc = new CalculadoraApp.Calculadora();
+
+            var resultado = calc.Dividir(a, b);
+
+            resultado.Should().Be(esperado);
+        }
+    }
+}
---
+++ CalculadoraApp/Calculadora.cs
@@ -0,0 +1,18 @@
+﻿namespace CalculadoraApp
+{
+    public class Calculadora
+    {
+        public int Somar(int a, int b) => a + b;
+
+        public int Subtrair(int a, int b) => a - b;
+
+        public int Multiplicar(int a, int b) => a * b;
+
+        public double Dividir(int a, int b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Não é possível dividir por zero.");
+            return (double)a / b;
+        }
+    }
+}
